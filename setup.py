86cea76c5e55b7a6fc06beca09beb65ef6502e5a
abdur razzak <ar@selftics.com>
2018-04-29 16:39:30 +0600

    package name chaged

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = 'mypackage',
-    mypackages = ['mypackage'],
+    name = 'selftics-py-pack',
+    selftics-py-packs = ['selftics-py-pack'],
     version = '0.1',
     description = 'A random test lib',
     author = 'Abdur Razzak',
     author_email = 'a.razzak660@gmail.com',
     url = 'https://github.com/ar012/mypackage',
-    download_url = 'https://github.com/ar012/mypackage/archive/0.1.tar.gz',
+    download_url = 'https://github.com/ar012/selftics-py-pack/archive/0.1.tar.gz',
     keywords = ['testing', 'logging', 'example'],
     classifiers = [],
 )
