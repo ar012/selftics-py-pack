46ffd08182486ec2c11064d022896129d316185c
abdur razzak <ar@selftics.com>
2018-04-29 16:48:25 +0600

    url changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description = 'A random test lib',
     author = 'Abdur Razzak',
     author_email = 'a.razzak660@gmail.com',
-    url = 'https://github.com/ar012/mypackage',
+    url = 'https://github.com/ar012/selftics-py-pack',
     download_url = 'https://github.com/ar012/selftics-py-pack/archive/0.1.tar.gz',
     keywords = ['testing', 'logging', 'example'],
     classifiers = [],
