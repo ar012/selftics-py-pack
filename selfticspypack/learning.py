f9bd46e1d938d47cf66a029aabdf091dd2eeb02c
abdur razzak <ar@selftics.com>
2018-04-29 17:09:12 +0600

    packge name chaged

---
+++ selfticspypack/learning.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+print("we love python")
+print('bangladesh is a small country. It is beautiful country.')
+print('bangladesh is a small country.\tIt is beautiful country.')
+
+input("give your country name: ")
+
+print("The End")
